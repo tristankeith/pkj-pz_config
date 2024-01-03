53af37d05ed3e71894f9d05e82dd78cb4bdd8653
JPBarcenas25 <54169731+jpb25@users.noreply.github.com>
2024-01-03 21:45:46 +0800

    Updated Custom Spawn

--- PKJ_spawnregions.lua
+++ PKJ_spawnregions.lua
@@ -4,6 +4,6 @@ function SpawnRegions()
 		{ name = "Riverside, KY", file = "media/maps/Riverside, KY/spawnpoints.lua" },
 		{ name = "Rosewood, KY", file = "media/maps/Rosewood, KY/spawnpoints.lua" },
 		{ name = "West Point, KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
-		{ name = "End of the Road, KY", serverfile = "X:/PZDedicated/Server_2024/Server/PKJ_spawnpoints.lua" },
+		{ name = "End of the Road, KY", serverfile = "PKJ_spawnpoints.lua" },
 	}
 end
