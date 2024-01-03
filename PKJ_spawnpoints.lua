14cb18b638aea6d50c7ad50624f1a754760fd8bb
JPBarcenas25 <54169731+jpb25@users.noreply.github.com>
2024-01-03 22:57:44 +0800

    Refactor spawnpoints in PKJ_spawnpoints.lua

--- PKJ_spawnpoints.lua
+++ PKJ_spawnpoints.lua
@@ -1,40 +1,35 @@
 function SpawnPoints()
-return {
-constructionworker = {
-    { worldX = 35, worldY = 30, posX = 105, posY = 148, posZ = 0 },
-    { worldX = 35, worldY = 30, posX = 103, posY = 4, posZ = 0 },
-    { worldX = 35, worldY = 30, posX = 274, posY = 67, posZ = 0 },
-    { worldX = 35, worldY = 30, posX = 298, posY = 71, posZ = 0 }
-},
-fireofficer = {
-    { worldX = 35, worldY = 30, posX = 105, posY = 148, posZ = 0 },
-    { worldX = 35, worldY = 30, posX = 103, posY = 4, posZ = 0 },
-    { worldX = 35, worldY = 30, posX = 274, posY = 67, posZ = 0 },
-    { worldX = 35, worldY = 30, posX = 298, posY = 71, posZ = 0 }
-},
-parkranger = {
-    { worldX = 35, worldY = 30, posX = 105, posY = 148, posZ = 0 },
-    { worldX = 35, worldY = 30, posX = 103, posY = 4, posZ = 0 },
-    { worldX = 35, worldY = 30, posX = 274, posY = 67, posZ = 0 },
-    { worldX = 35, worldY = 30, posX = 298, posY = 71, posZ = 0 }
-},
-policeofficer = {
-    { worldX = 35, worldY = 30, posX = 105, posY = 148, posZ = 0 },
-    { worldX = 35, worldY = 30, posX = 103, posY = 4, posZ = 0 },
-    { worldX = 35, worldY = 30, posX = 274, posY = 67, posZ = 0 },
-    { worldX = 35, worldY = 30, posX = 298, posY = 71, posZ = 0 }
-},
-securityguard = {
-    { worldX = 35, worldY = 30, posX = 105, posY = 148, posZ = 0 },
-    { worldX = 35, worldY = 30, posX = 103, posY = 4, posZ = 0 },
-    { worldX = 35, worldY = 30, posX = 274, posY = 67, posZ = 0 },
-    { worldX = 35, worldY = 30, posX = 298, posY = 71, posZ = 0 }
-},
-unemployed = {
-    { worldX = 35, worldY = 30, posX = 105, posY = 148, posZ = 0 },
-    { worldX = 35, worldY = 30, posX = 103, posY = 4, posZ = 0 },
-    { worldX = 35, worldY = 30, posX = 274, posY = 67, posZ = 0 },
-    { worldX = 35, worldY = 30, posX = 298, posY = 71, posZ = 0 }
-}
-}
+    return {
+        constructionworker = {
+            {worldX = 35, worldY = 30, posX = 105, posY = 148, posZ = 0},
+            {worldX = 35, worldY = 30, posX = 103, posY = 4, posZ = 0},
+            {worldX = 35, worldY = 30, posX = 274, posY = 67, posZ = 0},
+            {worldX = 35, worldY = 30, posX = 298, posY = 71, posZ = 0}
+        }, fireofficer = {
+            {worldX = 35, worldY = 30, posX = 105, posY = 148, posZ = 0},
+            {worldX = 35, worldY = 30, posX = 103, posY = 4, posZ = 0},
+            {worldX = 35, worldY = 30, posX = 274, posY = 67, posZ = 0},
+            {worldX = 35, worldY = 30, posX = 298, posY = 71, posZ = 0}
+        }, parkranger = {
+            {worldX = 35, worldY = 30, posX = 105, posY = 148, posZ = 0},
+            {worldX = 35, worldY = 30, posX = 103, posY = 4, posZ = 0},
+            {worldX = 35, worldY = 30, posX = 274, posY = 67, posZ = 0},
+            {worldX = 35, worldY = 30, posX = 298, posY = 71, posZ = 0}
+        }, policeofficer = {
+            {worldX = 35, worldY = 30, posX = 105, posY = 148, posZ = 0},
+            {worldX = 35, worldY = 30, posX = 103, posY = 4, posZ = 0},
+            {worldX = 35, worldY = 30, posX = 274, posY = 67, posZ = 0},
+            {worldX = 35, worldY = 30, posX = 298, posY = 71, posZ = 0}
+        }, securityguard = {
+            {worldX = 35, worldY = 30, posX = 105, posY = 148, posZ = 0},
+            {worldX = 35, worldY = 30, posX = 103, posY = 4, posZ = 0},
+            {worldX = 35, worldY = 30, posX = 274, posY = 67, posZ = 0},
+            {worldX = 35, worldY = 30, posX = 298, posY = 71, posZ = 0}
+        }, unemployed = {
+            {worldX = 35, worldY = 30, posX = 105, posY = 148, posZ = 0},
+            {worldX = 35, worldY = 30, posX = 103, posY = 4, posZ = 0},
+            {worldX = 35, worldY = 30, posX = 274, posY = 67, posZ = 0},
+            {worldX = 35, worldY = 30, posX = 298, posY = 71, posZ = 0}
+        }
+    }
 end
