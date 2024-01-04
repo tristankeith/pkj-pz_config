2c45192c7d17d15101e5b92163c86ca0e2657234
JPBarcenas25 <54169731+jpb25@users.noreply.github.com>
2024-01-04 16:47:37 +0800

    Update spawn point for unemployed

--- PKJ_spawnpoints.lua
+++ PKJ_spawnpoints.lua
@@ -1,22 +1,7 @@
 function SpawnPoints()
     return {
-        constructionworker = {
-            {worldX = 34, worldY = 25, posX = 50, posY = 210}
-        },
-        fireofficer = {
-            {worldX = 34, worldY = 25, posX = 50, posY = 210}
-        },
-        parkranger = {
-            {worldX = 34, worldY = 25, posX = 50, posY = 210}
-        },
-        policeofficer = {
-            {worldX = 34, worldY = 25, posX = 50, posY = 210}
-        },
-        securityguard = {
-            {worldX = 34, worldY = 25, posX = 50, posY = 210}
-        },
         unemployed = {
-            {worldX = 34, worldY = 25, posX = 50, posY = 210}
+            {worldX = 35, worldY = 29, posX = 50, posY = 210, posZ = 0}
         }
     }
 end
