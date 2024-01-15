efef1ff5eb3890bfff760ba88562184811292524
JPBarcenas25 <54169731+jpb25@users.noreply.github.com>
2024-01-15 13:30:27 +0800

    Remove unused spawnpoints and comment out PKJ spawn region

--- PKJ_spawnpoints.lua
+++ PKJ_spawnpoints.lua
@@ -2,9 +2,6 @@ function SpawnPoints()
     return {
         unemployed = {
             { worldX = 35, worldY = 29, posX = 50, posY = 210, posZ = 0 },
-            { worldX = 35, worldY = 29, posX = 50, posY = 155, posZ = 0 },
-            { worldX = 35, worldY = 29, posX = 50, posY = 50, posZ = 0 },
-            { worldX = 35, worldY = 29, posX = 50, posY = 75, posZ = 0 },
         },
     }
 end
