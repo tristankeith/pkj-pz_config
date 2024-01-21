3c7e3f2f0b032d542f12b1d47ebd31f5d9408350
JPBarcenas25 <54169731+jpb25@users.noreply.github.com>
2024-01-21 21:13:23 +0800

    Update XpMultiplier value to 2.0

--- PKJ_SandboxVars.lua
+++ PKJ_SandboxVars.lua
@@ -185,7 +185,7 @@ SandboxVars = {
     -- Number of days until 100% growth. -1 means no growth. Zero means use the Erosion Speed option. Maximum 36,500 (100 years). Minimum=-1 Maximum=36500 Default=0
     ErosionDays = 0,
     -- Modifies the base XP gain from actions by this number. Minimum=0.00 Maximum=1000.00 Default=1.00
-    XpMultiplier = 1.0,
+    XpMultiplier = 2.0,
     -- Determines if the XP multiplier affects passively levelled skills eg. Fitness and Strength.
     XpMultiplierAffectsPassive = true,
     -- Use this to multiply or reduce engine general loudness. Minimum=0.00 Maximum=100.00 Default=1.00
